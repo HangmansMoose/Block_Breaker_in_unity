a91d56ed137ec194746b2c1eec616e57541e4e2a
TrivialPC <trivial86@protonmail.com>
2018-11-25 15:37:35 +1100

    More Physics and collision box to trigger game over

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour {
+
+	//As defined in the unity docs for trigger events
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		//When collision box is hit trigger game over screen
+		SceneManager.LoadScene("Game Over");
+	}
+}
